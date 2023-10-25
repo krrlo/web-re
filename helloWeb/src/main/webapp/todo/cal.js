3c4bd9b24da5c7ad7a8156f218b4dda1118063ff
admin <admin@DESKTOP-0DADUM6>
2023-10-25 17:49:19 +0900

    오늘 수업 완료

---
+++ helloWeb/src/main/webapp/todo/cal.js
@@ -0,0 +1,67 @@
+//cal.js
+
+
+
+//makeCalendar();
+
+
+
+const h = ['일','월','화','수','목','금','토'];
+
+
+function makehead(h = []){           //얘를 html에 넣기 
+	let head = '';
+    head += '<table border = 1>';
+    head += '<thead>';	
+    head += '<tr>';
+	h.forEach(function(item){
+		head += makeTr(item);
+	})
+	head += '</tr>';
+	head +='</thead>';
+	
+	console.log(head)
+	document.getElementById('show').innerHTML += head;
+}
+
+
+
+
+function makeTr(member) {        
+		let html = '';                  
+		
+		html += '<th>' + member + '</th>'
+		
+		return html;
+	}
+
+
+makehead(h);  //h넣어서 월화수목금토일 헤드만듬
+
+/////////////
+
+
+
+function makebody() {
+	let body = '';
+	body += '<tbody>';
+	body += '<tr>';
+	for (let i = 1; i <= 31; i++) {
+		body += '<td>' + i + '</td>'
+		if(i % 7){
+			body += '<br>'
+		}
+	}
+	body += '</tr>';
+	body += '</tbody>';
+	
+	console.log("dfdfd" ,body)
+	document.getElementById('show').innerHTML += body;
+
+}
+
+makebody();
+
+
+
+//makeCalendar(); //함수호출하면 화면에 
